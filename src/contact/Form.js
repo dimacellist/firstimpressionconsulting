51d0069f4dd05eaa45aff247aaa936a138960327
dimacellist <dyanovyanovsky@gmail.com>
2019-08-31 23:18:44 +0300

    Some additions for the form

--- src/contact/Form.js
+++ src/contact/Form.js
@@ -1,60 +1,14 @@
 import React from 'react'
-import Axios from 'axios'
 
-const API_PATH = 'http://localhost:1992/my-reactapp/api/contact/index.php'
-
-class Form extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      name: '',
-      email: '',
-      message: '',
-      mailSent: false,
-      error: null
-    }
-  }
-
-  handleFormSubmit = e => {
-    e.preventDefault()
-    Axios({
-      method: 'post',
-      url: `${API_PATH}`,
-      headers: { 'content-type': 'application/json'},
-      data: this.state
-    })
-
-    .then(result => {
-      this.setState({
-        mailSent: result.data.sent
-      })
-    })
-    .catch(error => this.setState({
-      error: error.message
-    }))
-  }
-
-  render() {
-    return(
-      <form method="post" action="../mailer.php" className="form">
+function Form() {
+  return(
+    <form method="POST" action="../mailer.php" className="form">
       <div className="row">
         <div className="col span-1-of-3">
           <label>Name</label>
         </div>
         <div className="col span-2-of-3">
-          <input 
-            type="text" 
-            name="name" 
-            id="name" 
-            placeholder="Your name" 
-            required 
-            value={this.state.name}
-            onChange = {
-              e => this.setState({
-                name: e.target.value
-              })
-            }
-          />
+          <input type="text" name="name" id="name" placeholder="Your name" required />
         </div>
       </div>		
       <div className="row">
@@ -62,19 +16,7 @@ class Form extends React.Component {
           <label>Email</label>
         </div>
         <div className="col span-2-of-3">
-          <input 
-            type="email" 
-            name="email" 
-            id="email" 
-            placeholder="Your email" 
-            required 
-            value={this.state.email}
-            onChange = {
-              e => this.setState({
-                email: e.target.value
-              })
-            }
-          />
+          <input type="email" name="email" id="email" placeholder="Your email" required />
         </div>
       </div>			
       <div className="row">
@@ -82,17 +24,7 @@ class Form extends React.Component {
           <label>Send a message</label>
         </div>
         <div className="col span-2-of-3">
-          <textarea 
-            name="message" 
-            id="message" 
-            placeholder="Your message"
-            value={this.state.message}
-            onChange = {
-              e => this.setState({
-                message: e.target.value
-              })
-            }>
-          </textarea>
+          <textarea name="message" placeholder="Your message"></textarea>
         </div>
       </div>		
       <div className="row">
@@ -100,23 +32,11 @@ class Form extends React.Component {
             <label>&nbsp;</label>
         </div>
         <div className="col span-2-of-3">
-          <input 
-            type="submit" 
-            value="SUBMIT"
-            onClick = {
-              e => this.handleFormSubmit(e)
-            } 
-          />
-        </div>
-        <div className="col span-2-of-3">
-          {this.state.mailSent &&
-            <p>Thank you, Your message Has been Sent</p>
-          }
+          <input type="submit" value="SUBMIT" />
         </div>
       </div>  
     </form>
-    )
-  }
+  )
 }
 
 export default Form
